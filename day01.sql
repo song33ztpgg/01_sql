81eb206ca68ab9b9195a9318a750455e93332fc9
song <sasasa7737@gmail.com.com>
2019-05-03 17:02:51 +0900

    day01.sql

--- day01.sql
+++ day01.sql
@@ -1,1 +1,11 @@
-# SQL day 01 강의노트
+# SQL day 01 강의노트 
+
+--1)세션 설정 변경: 명령어 ctel + enter로 실행 
+--ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+--2)SCOOT 계정 생성 :F5로 실행 (스크립트 실행) 
+--@C:\app\13\product\18.0.0\dbhomeXE\rdbms\admin\scott.sql 
+--3)SCOTT접속을 생성 
+--ID:SCOTT 
+--PW:TIGER 
+--4)SCOTT 계정 접속후 실행명령 
+--scott계정의 모든 테이블 이름 조회
